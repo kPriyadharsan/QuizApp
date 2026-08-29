11d873687e318f88ec8d450c884198669ed2adaa
dharsanxo <dharsanoppoben@gmail.com>
2026-08-29 20:07:11 +0530

    Link updated

--- backend/index.js
+++ backend/index.js
@@ -46,6 +46,7 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
     'https://xo-quiz.vercel.app',
+    'https://svhec-ece-assessments.vercel.app',
 ].filter(Boolean);
 
 app.use(cors({
